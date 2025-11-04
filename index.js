ae3fda11ffd3e0deb89ad8500e56da287365d819
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-11-04 14:06:22 +0700

    menambahkan endpoint post untuk menambahkan data baru

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+const kandang = require('./models/kandang');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -8,4 +9,23 @@ app.use(express.urlencoded({ extended: false}));
 
 app.listen(PORT, async() => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+    console.log(err);
+})
+
+app.post('/kandang', async (req, res) => {
+    const data = req.body;
+    try{
+        const kandang = await db.kandang.create(data);
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
 });
